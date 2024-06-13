2cc35d283e844b28533db578794d420affa68b39
vopno__ <beserraguilherme405@gmail.com>
2024-06-13 18:38:49 -0300

    Jogo de quis concluido, todos os sistemas feitos com sucesso

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -13,14 +13,19 @@ let prgIniciativa = 0;
 let prgDesigualdade = 0;
 
 //Variaveis que definem quantas perguntas de cada categoria tem
-let nCientistasFamosas = 2;
-let nDescobertas = 2;
-let nDesigualdade = 2;
-let nIniciativas = 3;
+let nCientistasFamosas = 6;
+let nDescobertas = 6;
+let nDesigualdade = 4;
+let nIniciativas = 6;
 let nTodas = nCientistasFamosas + nDescobertas + nDesigualdade + nIniciativas;
 
 function inicioGame(){
     $('.play').remove();
+    $('#contButtonsInicial').classList.add('tam0')
+
+    $('.cont-button-inicial').classList.remove('cont-button-inicial');
+
+
     $('.return').classList.add('novaPosicao');
     introduction();
 }
@@ -64,19 +69,22 @@ function addEvent (){
 }
 
 function introduction(){
+    let i = 0;
 
-    let i = 0 
     const mensageWelcome = ['Bem vindo ao meu trabalho do Ceara Cientifico'];
 
     const divCont = createElement('div');
     const buttonAvancar = createElement('button');
+    const h2 = createElement('h2');
 
-    buttonAvancar.innerText = '>';
+    buttonAvancar.innerHTML = '<img src="./assets/img/jogos/avancar.svg" class="img-button-avancar"></img>';
     buttonAvancar.classList.add('avancar');
     buttonAvancar.addEventListener('click', () => {
         if(i < mensageWelcome.length-1){
             i++
-            divCont.innerText = mensageWelcome[i]
+            
+            h2.innerText = mensageWelcome[i]
+            divCont.appendChild(h2)
             divCont.appendChild(buttonAvancar)
         } else {
             $('.div-cont-inicio').remove()
@@ -85,7 +93,8 @@ function introduction(){
         }
     })
     
-    divCont.innerText = mensageWelcome[i];
+    h2.innerText = mensageWelcome[i];
+    divCont.appendChild(h2)
     divCont.classList.add('div-cont-inicio');
     divCont.appendChild(buttonAvancar);
 
@@ -107,10 +116,13 @@ function perguntas(categoria, removed = false) {
 
         const p = createElement('p');
         const divCont = createElement('div');
+        const divButton = createElement('div');
 
         p.innerText = `Pergunta ${nPergunta + 1} - ${enunciado}`;
         divCont.appendChild(p);
         divCont.classList.add('cont-perguntas');
+
+        divButton.classList.add('cont-buttons');
 
         alternativas.forEach((alternativa, i) => {
             const orde = ['a', 'b', 'c', 'd'];
@@ -118,18 +130,19 @@ function perguntas(categoria, removed = false) {
             button.classList.add('button-escolha');
             button.innerText = `${orde[i].toUpperCase()}) ${alternativa}`;
             button.setAttribute('id', orde[i]);
-            divCont.appendChild(button);
+            divButton.appendChild(button);
         });
 
+        divCont.appendChild(divButton);
         $('.cont-game').appendChild(divCont);
 
         const escolhas = document.querySelectorAll('.button-escolha');
         escolhas.forEach(clicado => {
             clicado.addEventListener('click', () => {
                 if (verificarPergunta(clicado.id, resposta, alternativas)) {
-                    respostaResultado(true);
+                    respostaResultado(true, info);
                 } else {
-                    respostaResultado(false);
+                    respostaResultado(false, info);
                 }
             });
         });
@@ -179,25 +192,30 @@ function verificarPergunta(resposta, respostaCerta, alternativas){
     }
 }
 
-function respostaResultado(resu){
+function respostaResultado(resu, info){
     $('.cont-perguntas').remove();
 
     const divContResu = createElement('div');
     const p = createElement('p');
+    const p2 = createElement('p');
+
+    p2.innerHTML = `<em>Info: ${info}</em>`
 
     divContResu.classList.add('cont-resu')
 
     if(resu){
-        p.innerText = 'Acertou fdp'
+        p.innerHTML = 'Reposta <span class="verde-win">correta</span>'
         p.classList.add('resposta-certa')
         updateScore(1);
 
     } else {
-        p.innerText = 'Errou fdp';
+        p.innerHTML = 'Resposta <span class="vermelho-loser">errada</span>';
         p.classList.add('resposta-errada');    
     }
 
     divContResu.appendChild(p);
+
+    divContResu.appendChild(p2);
     $('.cont-game').appendChild(divContResu);
 
     nextQuestion();
@@ -230,11 +248,22 @@ function updateScore(i){
 }
 
 function acabouPerguntas(){
-    const finalParagraph = createElement('p');
-    finalParagraph.innerText = `Acabou o jogo, parabens você fez ${score} de ${nPergunta}. Tendo uma media de ${nPergunta / score}`;
 
+    $('.cont-resu').remove();
+
+    const p = ['Fim de jogo!', `Parabéns você acertou ${score} de ${nPergunta} perguntas. Tendo uma media de acertos de ${(nPergunta / score).toFixed(2)}!`, 'Obrigado por participar']; // Mensagens a serem criadas quando o jogo acabar 
+
+    const divCont = createElement('div');
+
+    for(let i in p){
+        const p1 = createElement('p');
+        p1.innerHTML = `${p[i]}<br>`;
+        divCont.appendChild(p1);
+    }
+
+    divCont.classList.add('cont-resu-final');
     jaAcabou = true;
-    $('.cont-game').appendChild(finalParagraph);
+    $('.cont-game').appendChild(divCont);
 }
 
 function bancoDePerguntas(ctg, prg) {
@@ -268,7 +297,31 @@ function bancoDescobertas(prg) {
             alternativas: ['Rita Levi-Montalcini', 'Rosalyn Yalow', 'Maria Goeppert Mayer', 'Dorothy Crowfoot Hodgkin'],
             resposta: 'Rosalyn Yalow',
             info: 'Rosalyn Yalow ganhou o Prêmio Nobel de Medicina em 1977 por seu trabalho no desenvolvimento de radioimunoensaio.'
-        }
+        },
+        {
+            enunciado: "Quem desenvolveu as técnicas de cristalografia de raios X que levaram à descoberta da estrutura do DNA?",
+            alternativas: ["Lise Meitner", "Chien-Shiung Wu", "Rosalind Franklin", "Barbara McClintock"],
+            resposta: "Rosalind Franklin",
+            info: "Rosalind Franklin fez contribuições cruciais para a descoberta da estrutura do DNA usando cristalografia de raios X."
+        },
+        {
+            enunciado: "Quem descobriu os transposons (genes saltadores) em milho, ganhando o Prêmio Nobel de Fisiologia ou Medicina?",
+            alternativas: ["Barbara McClintock", "Gerty Cori", 'Linda Buck', "Carol Greider"],
+            resposta: "Barbara McClintock",
+            info: "Barbara McClintock recebeu o Nobel em 1983 por suas descobertas sobre os transposons."
+        },
+        {
+            enunciado: "Quem foi a bioquímica que recebeu o Prêmio Nobel por suas pesquisas sobre a glicose e o metabolismo, juntamente com seu marido Carl Cori?",
+            alternativas: ["Gertrude B. Elion", "Gerty Cori", "Rita Levi-Montalcini", "Elizabeth Blackburn"],
+            resposta: "Gerty Cori",
+            info: "Gerty Cori ganhou o Prêmio Nobel de Fisiologia ou Medicina em 1947 por seu trabalho no ciclo de Cori, que descreve a conversão de glicose em energia no corpo."
+        },
+        {
+            enunciado: "Quem foi a física nuclear que trabalhou na separação do isótopo de urânio-235 e participou do Projeto Manhattan?",
+            alternativas: ["Lise Meitner", "Chien-Shiung Wu", "Maria Goeppert Mayer", "Leona Woods"],
+            resposta: "Chien-Shiung Wu",
+            info: "Chien-Shiung Wu fez contribuições importantes para o desenvolvimento da física nuclear e é conhecida por seu trabalho na verificação da teoria da paridade."
+        }        
     ];
 
     if (prg < descobertas.length) {
@@ -292,7 +345,34 @@ function bancoCientistaFamosas(prg) {
             alternativas: ['Irène Joliot-Curie', 'Lise Meitner', 'Marie Curie', 'Rosalind Franklin'],
             resposta: 'Marie Curie',
             info: 'Marie Curie, junto com seu marido Pierre Curie, descobriu o rádio e o polônio em 1898.'
-        }
+        },
+        {
+            enunciado: 'Quem foi a primeira mulher a ser eleita para a Academia Nacional de Ciências dos Estados Unidos?',
+            alternativas: ['Barbara McClinton', 'Florence Sabin', 'Rachel Carson', 'Maria Goeppert Mayer'],
+            resposta: 'Florence Sabin',
+            info: 'Florence Sabin foi eleita em 1925, sendo a primeira mulher a entrar na Academia Nacional de Ciências dos EUA.'
+        },
+        {
+            enunciado: 'Qual cientista britânica ganhou o Prêmio Nobel de Química em 1964 por suas descobertas sobre a estrutura da penicilina e da vitamina B12?',
+            alternativas: ['Dorothy Crowfoot Hodgkin', 'Rosalind Franklin', 'Ada Yonath', 'Frances Arnold'],
+            resposta: 'Dorothy Crowfoot Hodgkin',
+            info: 'Dorothy Hodgkin fez contribuições significativas para a cristalografia de raios X, elucidando as estruturas da penicilina e da vitamina B12.'
+        },
+        {
+            enunciado: "Quem foi a primeira mulher a ganhar um Prêmio Nobel?",
+            alternativas: ["Rosalind Franklin", "Marie Curie", "Ada Lovelace", "Dorothy Hodgkin"],
+            resposta: "Marie Curie",
+            info: "Marie Curie ganhou o Prêmio Nobel de Física em 1903 e o de Química em 1911."
+        },
+        {
+            enunciado: "Qual cientista britânica descobriu a estrutura do DNA, mas não recebeu o Nobel?",
+            alternativas: ["Dorothy Hodgkin", "Ada Lovelace", "Rosalind Franklin", "Lise Meitner"],
+            resposta: "Rosalind Franklin",
+            info: "Rosalind Franklin fez descobertas cruciais sobre a estrutura do DNA, mas não foi reconhecida com o Prêmio Nobel que foi concedido a seus colegas Watson e Crick."
+        },
+        
+        
+        
     ];
 
     if (prg < cientistaFamosas.length) {
@@ -322,6 +402,24 @@ function bancoIniciativas(prg) {
             alternativas: ['Horizon 2020', 'Erasmus+', 'Marie Skłodowska-Curie Actions', 'Creative Europe'],
             resposta: 'Marie Skłodowska-Curie Actions',
             info: 'O programa Marie Skłodowska-Curie Actions apoia pesquisadores em todas as etapas de suas carreiras e promove a igualdade de gênero.'
+        },
+        {
+            enunciado: "Qual é a meta da ONU Mulheres para alcançar a igualdade de gênero no mundo até?",
+            alternativas: ["2025", "2030", "2040", "2050"],
+            resposta: "2030",
+            info: "A ONU Mulheres visa alcançar a igualdade de gênero e empoderar todas as mulheres e meninas até 2030, como parte dos Objetivos de Desenvolvimento Sustentável (ODS)."
+        },
+        {
+            enunciado: "Qual programa da NASA visa incentivar mais meninas a se interessarem por ciência e tecnologia?",
+            alternativas: ["STEM Girls", "Girls in Space", 'NASA GIRLS', "Women in STEM"],
+            resposta: "NASA GIRLS",
+            info: "O programa NASA GIRLS (e BOYS) conecta meninas e meninos do ensino fundamental com mentores da NASA para promover a educação em STEM."
+        },
+        {
+            enunciado: "Qual organização lançou o programa 'Marie Curie Actions' para apoiar pesquisadores?",
+            alternativas: ["NASA", "CERN", "European Commission", "UNESCO"],
+            resposta: "European Commission",
+            info: "O programa 'Marie Curie Actions' da Comissão Europeia apoia pesquisadores em todas as etapas de suas carreiras, promovendo a mobilidade e a excelência na pesquisa."
         }
     ];
 
@@ -346,7 +444,21 @@ function bancoDesigualdade(prg) {
             alternativas: ['1940s', '1950s', '1960s', '1970s'],
             resposta: '1970s',
             info: 'As principais universidades de ciências começaram a admitir mulheres em maior número na década de 1970.'
+        },
+        {
+            enunciado: "Em média, qual é a diferença percentual de salários entre homens e mulheres em cargos de ciência e tecnologia?",
+            alternativas: ["10%", "15%", "20%", "25%"],
+            resposta: "20%",
+            info: "Estudos mostram que, em média, mulheres ganham cerca de 20% a menos que seus colegas homens em cargos similares em ciência e tecnologia."
+        },
+        {
+            enunciado: "Qual é o percentual aproximado de mulheres nas engenharias, segundo dados recentes?",
+            alternativas: ["10%", "15%", "20%", "25%"],
+            resposta: "15%",
+            info: "As mulheres continuam sub-representadas nas engenharias, com cerca de 15% de participação."
         }
+        
+        
     ];
 
     if (prg < desigualdadeDeGenero.length) {
